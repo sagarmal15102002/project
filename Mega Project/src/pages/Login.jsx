19825ded3c44f6ba184f8fea2aa25e33fedc2b15
Sagarmal kuldiya <163585142+sagarmal15102002@users.noreply.github.com>
2024-09-10 23:05:22 -0700

    Add files via upload

---
+++ Mega Project/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Login } from 'd:/React code/12thmegablok/12MegaBlog/src/components/index'
+function Login() {
+  return (
+    <div className='py-8'>
+        <Login />
+    </div>
+  )
+}
+
+export default Login
